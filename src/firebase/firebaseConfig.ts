765d60af4ceefbebb983aa1fc4f2cc129eee832a
mtatsuki3 <mtaki0318@gmail.com>
2026-08-30 00:59:27 +0900

    Add Firebase authentication storage and firestore

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAODy5sI--XKea1f_yRpaW2ujYQV-_Xu40",
+  authDomain: "magonews-571c1.firebaseapp.com",
+  projectId: "magonews-571c1",
+  storageBucket: "magonews-571c1.firebasestorage.app",
+  messagingSenderId: "681751269117",
+  appId: "1:681751269117:web:3807de249fa7a650ba3894",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
